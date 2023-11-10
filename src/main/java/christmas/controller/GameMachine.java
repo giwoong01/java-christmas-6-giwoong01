d85bd691c94762bf4270ab53b014905efb03af51
giwoong <dev.choi0409@gmail.com>
2023-11-10 16:00:39 +0900

    feat: Buyer 구현

--- src/main/java/christmas/controller/GameMachine.java
+++ src/main/java/christmas/controller/GameMachine.java
@@ -1,6 +1,8 @@
 package christmas.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Buyer;
+import java.util.Arrays;
 
 public class GameMachine {
 
@@ -11,9 +13,13 @@ public class GameMachine {
         int inputDate = Integer.parseInt(Console.readLine());
 
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String inputMenu = Console.readLine();
+        String inputMenuAndCount = Console.readLine();
+        String[] menuAndCounts = inputMenuAndCount.split(",");
+        Buyer buyer = new Buyer(Arrays.stream(menuAndCounts).toList());
 
         System.out.printf("12월 %d일 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n", inputDate);
 
+        System.out.println("\n<주문 메뉴>");
+        System.out.println(buyer);
     }
 }
---
+++ src/main/java/christmas/domain/Buyer.java
@@ -0,0 +1,24 @@
+package christmas.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Buyer {
+    private List<String> menuAndCounts;
+
+    public Buyer(List<String> menuOfNumbers) {
+        this.menuAndCounts = menuOfNumbers;
+    }
+
+    @Override
+    public String toString() {
+        return menuAndCounts.stream()
+                .map(this::formatMenuAndCount)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private String formatMenuAndCount(String menuOfNumber) {
+        String[] split = menuOfNumber.split("-");
+        return String.format("%s %s개", split[0], split[1]);
+    }
+}
