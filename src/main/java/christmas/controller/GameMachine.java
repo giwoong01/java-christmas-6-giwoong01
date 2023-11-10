2d894219846bcb952e538f23b4b378117855dee4
giwoong <dev.choi0409@gmail.com>
2023-11-10 15:32:30 +0900

    feat: 구매자 입력 구현

---
+++ src/main/java/christmas/controller/GameMachine.java
@@ -0,0 +1,19 @@
+package christmas.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameMachine {
+
+    public void start() {
+        // 구매자 입력
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플레너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        int inputDate = Integer.parseInt(Console.readLine());
+
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String inputMenu = Console.readLine();
+
+        System.out.printf("12월 %d일 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n", inputDate);
+
+    }
+}
